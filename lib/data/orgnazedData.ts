5bb69a7c1e43e6ee077fa34fecceb89c547b65ef
Elias Renawi <elias.renawi@bizi.co.il>
2024-04-15 16:59:47 +0300

    add some cesar data

--- lib/data/orgnazedData.ts
+++ lib/data/orgnazedData.ts
@@ -1,15 +1,18 @@
 import { beefBurgers } from "./beefBurgersData";
 import { chickenBurgers } from "./chickenBurgerData";
-import { onTop } from "./onTopData";
+//import { onTop } from "./onTopData";
+import { onTop } from "../newData/cesarOnTop";
+import { Classicburgers } from "../newData/classicBurgers";
 import { popular } from "./popularData";
 import { salads } from "./saladsData";
 import { sides } from "./sides";
 
 export const sections = [
-    { heading: 'Popular', meals: popular },
-    { heading: 'Beef Burgers', meals: beefBurgers },
-    { heading: 'Chicken Burgers', meals: chickenBurgers },
+    //{ heading: 'Popular', meals: popular },
+    //  { heading: 'Beef Burgers', meals: beefBurgers },
+    // { heading: 'Chicken Burgers', meals: chickenBurgers },
+    { heading: 'Classic', meals: Classicburgers },
     { heading: 'OnTop', meals: onTop },
-    { heading: 'Salads', meals: salads },
-    { heading: 'sides', meals: sides },
+    //  { heading: 'Salads', meals: salads },
+    //   { heading: 'sides', meals: sides },
 ] as const;
---
+++ lib/newData/cesarOnTop.ts
@@ -0,0 +1,54 @@
+import Jalapemos from "@/public/cesardata/ontop/jalapemos.png"
+import Pineapple from "@/public/cesardata/ontop/pineapple.png"
+import HotDogs from "@/public/cesardata/ontop/hotdogs.png"
+import Bacon from "@/public/cesardata/ontop/bacon.png"
+import FriedEgg from "@/public/cesardata/ontop/friedegg.png"
+import Mshrooms from "@/public/cesardata/ontop/mushrooms.png"
+import CreamyMushrooms from "@/public/cesardata/ontop/creamymushrooms.png"
+
+
+export const onTop = [
+
+    {
+        title: "JALAPENOS",
+        description: "JALAPENOS_DESCRIPTION",
+        imageUrl: Jalapemos,
+        price: "5.00 ₪",
+    },
+    {
+        title: "Pineapple",
+        description: "JALAPENOS_DESCRIPTION",
+        imageUrl: Pineapple,
+        price: "5.00 ₪",
+    },
+    {
+        title: "HotDogs",
+        description: "JALAPENOS_DESCRIPTION",
+        imageUrl: HotDogs,
+        price: "5.00 ₪",
+    },
+    {
+        title: "Bacon",
+        description: "JALAPENOS_DESCRIPTION",
+        imageUrl: Bacon,
+        price: "5.00 ₪",
+    },
+    {
+        title: "Fried Egg",
+        description: "JALAPENOS_DESCRIPTION",
+        imageUrl: FriedEgg,
+        price: "5.00 ₪",
+    },
+    {
+        title: "Mshrooms",
+        description: "JALAPENOS_DESCRIPTION",
+        imageUrl: Mshrooms,
+        price: "5.00 ₪",
+    },
+    {
+        title: "CreamyMushrooms",
+        description: "JALAPENOS_DESCRIPTION",
+        imageUrl: CreamyMushrooms,
+        price: "5.00 ₪",
+    },
+] as const;
